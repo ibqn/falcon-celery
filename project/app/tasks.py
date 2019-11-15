c4791cd6352fba97cf4e57e48a405cf1119a1576
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2019-11-15 21:16:05 +0100

    remove old code

--- project/app/tasks.py
+++ project/app/tasks.py
@@ -18,24 +18,6 @@ CELERY_BACKEND = os.environ.get('CELERY_BACKEND')
 app = celery.Celery('tasks', broker=CELERY_BROKER, backend=CELERY_BACKEND)
 cache = redis.Redis(host='redis', port=6379)
 
-# @app.task
-# def fib(num):
-#     '''Return a fibonacci sequence value of num'''
-#     # simulate slow computation
-#     # sleep(2)
-
-#     cached_result = cache.get(f'fib_{num}')
-#     if cached_result:
-#         return int(cached_result)
-
-#     if num == 0:
-#         return 0
-#     if num == 1 or num == 2:
-#         return 1
-
-#     result = fib(num - 2) + fib(num - 1)
-#     cache.set(f'fib_{num}', result)
-#     return result
 
 @app.task
 def fib(val):
